d3f6e8df5041f732c26362ba8256c22a4801cfe2
NoSpooksAllowed <hersgori4@protonmail.com>
2025-07-17 23:28:26 -0300

    updated feature for pokedex

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
-app.get('/version', (req, res) => {
-  res.send('4')
-})
+app.get("/version", (req, res) => {
+  res.send("4");
+});
 
-app.get('/health', (req, res) => {
-  res.send('ok')
-})
+app.get("/health", (req, res) => {
+  res.send("ok");
+});
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`)
-})
+  console.log(`server started on port ${PORT}`);
+});
